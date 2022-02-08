0aa8eef97cfe9bf3a3f36bdfee58f1e50f51157c
Jason A. Wise <jasonandrewwise@gmail.com>
2022-02-08 18:38:43 -0500

    Initial Commit

---
+++ WiseTip/WiseTipApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiseTipApp.swift
+//  WiseTip
+//
+//  Created by Jason Wise on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct WiseTipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
